a306a2f069e5549c05a74f492b1081a42b9675bb
LU CHIA HUNG <68722406+CarlosLu63@users.noreply.github.com>
2023-06-17 14:19:43 +0800

    Update Sanger_tracy_final_v2.py
    add progress info

--- Sanger_tracy_final_v2.py
+++ Sanger_tracy_final_v2.py
@@ -1,38 +1,55 @@
 import os
 import pandas as pd
 import glob
+import sys
 
 class Sanger_analysis(object):
 
-    def main_flow(self):
-
+    def main_flow(self, Chr, Pos, version):
+        
+        self.post_dict = {}
+        ###prepare checking list###
+        self.post_dict['chrom'] = str(Chr)
+        self.post_dict['posit'] = str(Pos)
+        
         ###screen ab1 file###
-        input_file = glob.glob(self.sample_path + "*.ab1")[0]
+        try:
+            input_file = glob.glob(self.sample_path + "*.ab1")
+        except Exception as e:
+            print("Unexpected error when screening ab1 file: ", e)
 
-        ###setup path name###
-        vcf_path, input_filename, output_filename = self.read_ab1(input_file)
+        n = 1
+        for f in input_file:
+            ###setup path name###
+            #vcf_path, input_filename, output_filename = self.read_ab1(input_file)
+            vcf_path, input_filename, output_filename = self.read_ab1(f)
+            print("Processing ", input_filename, "\nProgress: ", n, "/", len(input_file))
 
-        ###apply variant calling###
-        self.variant_calling(output_filename, input_filename)
+            ###apply variant calling###
+            self.variant_calling(output_filename, input_filename, version)
 
-        ###convert bcf to vcf###
-        vcf_out = self.bcf_to_vcf(output_filename, vcf_path)
-        
-        ###output all variant as csv file###
-        self.output_all_var(vcf_out, output_filename)
-        
-        ###check 10 pos###
-        sanger_check = self.check_pos(vcf_out)
+            ###convert bcf to vcf###
+            vcf_out = self.bcf_to_vcf(output_filename, vcf_path)
+            
+            ###output all variant as csv file###
+            self.output_all_var(vcf_out, output_filename)
+            
+            ###check pos###
+            check_result = self.check_pos(vcf_out)
 
-        ###print dictionary###
-        print(sanger_check)
+            ###print dictionary###
+            #print(check_result)
+            self.sanger_check = pd.DataFrame.from_dict(check_result, orient='index').transpose()
+            self.sanger_check.to_csv(self.output_path + output_filename + "/" + output_filename + "_sanger_check.csv", index = False, header = True)
+            n += 1    
 
 
     def __init__(self):
         self.path = "/home/carloslu/Sanger/"
-        self.sample_path = "/home/carloslu/Sanger/Sample_file/GJB2_cases/"
+        self.sample_path = "/home/carloslu/Sanger/Sample_file/WES_cases/"
         self.output_path = "/home/carloslu/Sanger/output_file/"
-        self.ref_path = "/home/carloslu/Sanger/Reference/Homo_sapiens.GRCh37.dna.primary_assembly.fa.gz"
+        self.ref_path37 = "/home/carloslu/Sanger/Reference/Homo_sapiens.GRCh37.dna.primary_assembly.fa.gz"
+        self.ref_path38 = "/home/carloslu/Sanger/Reference/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz"
         self.scriptDir = "/home/carloslu/Sanger/code/"
         self.tool_path = "/home/carloslu/Sanger/tracy/bin/tracy"
         self.bcftools_path = "/opt/bcftools/bcftools"
@@ -48,11 +65,18 @@ class Sanger_analysis(object):
         return vcf_path, input_filename, output_filename
 
 
-    def variant_calling(self, output_filename, input_filename):
+    def variant_calling(self, output_filename, input_filename, version):
+        #############################################
+        ###variant calling only forward or reverse###
+        #############################################
         if os.path.exists(self.output_path + output_filename) == False:
             os.mkdir(self.output_path + output_filename)
 
-        os.system(self.tool_path + " decompose -v -a homo_sapiens_hg19 -r " + self.ref_path + self.vc_parameter + " -o " + self.output_path + output_filename + "/" + output_filename + " "  + self.sample_path + input_filename)
+        if version == '37':
+            os.system(self.tool_path + " decompose -v -a homo_sapiens_hg19 -r " + self.ref_path37 + self.vc_parameter + " -o " + self.output_path + output_filename + "/" + output_filename + " "  + self.sample_path + input_filename)
+        elif version == '38':
+            os.system(self.tool_path + " decompose -v -a homo_sapiens -r " + self.ref_path38 + self.vc_parameter + " -o " + self.output_path + output_filename + "/" + output_filename + " "  + self.sample_path + input_filename)
+
         os.system("rm -f " + self.output_path +  output_filename + "/" + "*.align1")
         os.system("rm -f " + self.output_path +  output_filename + "/" + "*.align2")
         os.system("rm -f " + self.output_path +  output_filename + "/" + "*.align3")
@@ -104,34 +128,42 @@ class Sanger_analysis(object):
         sanger_all = pd.DataFrame.from_dict(sanger_allres).transpose()
         sanger_all.to_csv(self.output_path + output_filename + "/" + output_filename + "_allvariant.csv", index = False, header = True)    
 
-
-    ###checking 10 positions###
+    ###checking position###
     def check_pos(self, vcf_out):
         sanger_check = {}
-        for i in self.pos_list:
-            if i in vcf_out["ID"].values.tolist():
-                #print(i)
-                subdata = pd.DataFrame(vcf_out[vcf_out['ID']==i][["INFO", "sample"]])
-                type_ = subdata.iloc[0][0].split("=")[1].split(";")[0]
-                genotype = subdata.iloc[0][1].split(":")[0]
-                if genotype == '0/0':
-                    gt = 'Homozygous'
-                elif genotype == '0/1':
-                    gt = 'Heterozygous'
-                elif genotype == '1/1':
-                    gt = 'Homozygous ALT'
-                else:
-                    gt = 'NA'
-                #print(type_)
-                #print(gt)
-                sanger_check[i] = [type_, gt]
+        if self.post_dict['chrom'] in vcf_out["#CHROM"].values.tolist() and self.post_dict['posit'] in vcf_out["POS"].values.tolist():
+            #print(post_dict['posit'])
+            subdata = pd.DataFrame(vcf_out[vcf_out['POS'] == self.post_dict['posit']][["ID", "REF", "ALT", "INFO", "sample"]])
+            rs_ID = subdata.iloc[0][0]
+            alt_ref = subdata.iloc[0][1] + ' > ' + subdata.iloc[0][2]
+            type_ = subdata.iloc[0][3].split("=")[1].split(";")[0]
+            genotype = subdata.iloc[0][4].split(":")[0]
+            if genotype == '0/0':
+                gt = 'Homozygous'
+            elif genotype == '0/1':
+                gt = 'Heterozygous'
+            elif genotype == '1/1':
+                gt = 'Homozygous ALT'
             else:
-                sanger_check[i] = 'Normal'
+                gt = 'NA'    
+            sanger_check['Result'] = 'Found'
+            sanger_check['Chromosome'] = self.post_dict['chrom']
+            sanger_check['Position'] = self.post_dict['posit']
+            sanger_check['rs_ID'] = rs_ID
+            sanger_check['Alteration'] = alt_ref
+            sanger_check['Mutation type'] = type_
+            sanger_check['Genotype'] = gt
+        else:
+            sanger_check['Result'] = 'Not found'
+
         return sanger_check
 
-def main():	 
+def main():
+    Chr = sys.argv[1]
+    Pos = sys.argv[2]
+    version = sys.argv[3]
     main_obj = Sanger_analysis()
-    main_obj.main_flow()
+    main_obj.main_flow(Chr, Pos, version)
 
 if __name__ == "__main__":
 ### simple main
